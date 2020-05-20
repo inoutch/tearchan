3669114c1ae68c24fbaaedd6b2ddc803c5bf5f24
inoutch <akiandaki@gmail.com>
2020-05-21 02:12:25 +0900

    feat(examples): Add hello world example

---
+++ examples/app/hello_world_scene.rs
@@ -0,0 +1,109 @@
+use nalgebra_glm::vec3;
+use tearchan::core::graphic::batch::batch3d::Batch3D;
+use tearchan::core::graphic::batch::batch_buffer_f32::BatchBufferF32;
+use tearchan::core::graphic::batch::Batch;
+use tearchan::core::graphic::camera_3d::Camera3D;
+use tearchan::core::graphic::hal::backend::{GraphicPipeline, Texture};
+use tearchan::core::graphic::image::Image;
+use tearchan::core::graphic::polygon::{Polygon, PolygonCommon};
+use tearchan::core::graphic::shader::standard_3d_shader_program::Standard3DShaderProgram;
+use tearchan::core::scene::scene_base::SceneBase;
+use tearchan::core::scene::scene_context::SceneContext;
+use tearchan::core::scene::scene_creator::SceneCreator;
+use tearchan::extension::shared::Shared;
+use tearchan::math::mesh::MeshBuilder;
+
+pub struct HelloWorldScene {
+    camera: Camera3D,
+    batch: Batch<Polygon, BatchBufferF32, Batch3D<BatchBufferF32>>,
+    shader_program: Standard3DShaderProgram,
+    texture: Texture,
+    graphic_pipeline: GraphicPipeline,
+    polygon: Shared<Polygon>,
+}
+
+impl HelloWorldScene {
+    pub fn creator() -> Option<SceneCreator> {
+        Some(|scene_context| {
+            let screen_size = scene_context.renderer_api.screen_size();
+            let image = Image::new_empty();
+
+            let mut camera_3d = Camera3D::new(screen_size.x / screen_size.y);
+            camera_3d.position = vec3(0.0f32, -2.0f32, 4.0f32);
+            camera_3d.target_position = vec3(0.0f32, 0.0f32, 0.0f32);
+            camera_3d.up = vec3(0.0f32, 1.0f32, 0.0f32);
+            camera_3d.update();
+
+            //
+            let texture = scene_context.renderer_api.create_texture(&image);
+
+            let standard_3d_shader_program =
+                Standard3DShaderProgram::new(scene_context.renderer_api, camera_3d.base());
+            let graphic_pipeline_3d = scene_context
+                .renderer_api
+                .create_graphic_pipeline(standard_3d_shader_program.shader());
+
+            let mesh3 = MeshBuilder::new().with_cube(1.0f32).build().unwrap();
+            let mut batch_3d = Batch3D::new(scene_context.renderer_api);
+            let polygon_2d = Shared::new(Polygon::new(mesh3));
+            polygon_2d
+                .borrow_mut()
+                .set_rotation_axis(vec3(0.0f32, 1.0f32, 1.0f32));
+            batch_3d.add(&polygon_2d, 0);
+
+            Box::new(HelloWorldScene {
+                camera: camera_3d,
+                batch: batch_3d,
+                polygon: polygon_2d,
+                shader_program: standard_3d_shader_program,
+                graphic_pipeline: graphic_pipeline_3d,
+                texture,
+            })
+        })
+    }
+}
+
+impl SceneBase for HelloWorldScene {
+    fn update(&mut self, scene_context: &mut SceneContext, _delta: f32) {
+        self.polygon
+            .borrow_mut()
+            .set_rotation_radian(self.polygon.rotation_radian() + 0.01f32);
+        self.camera.update();
+        self.batch.flush();
+
+        self.shader_program.prepare(
+            self.camera.combine(),
+            &vec3(0.0f32, -2.0f32, 4.0f32),
+            &vec3(1.0f32, 1.0f32, 1.0f32),
+            0.2f32,
+            &self.texture,
+        );
+
+        let descriptor_set = self.graphic_pipeline.borrow_descriptor_set();
+        let write_descriptor_sets =
+            tearchan::core::graphic::shader::standard_3d_shader_program::write_descriptor_sets(
+                descriptor_set,
+                &self.shader_program.vp_matrix_uniform,
+                &self.shader_program.inv_vp_matrix_uniform,
+                &self.shader_program.light_position_uniform,
+                &self.shader_program.light_color_uniform,
+                &self.shader_program.ambient_strength_uniform,
+                self.texture.borrow_image_view(),
+                self.texture.borrow_sampler(),
+            );
+
+        scene_context
+            .renderer_api
+            .write_descriptor_sets(write_descriptor_sets);
+        scene_context.renderer_api.draw_triangle(
+            &self.graphic_pipeline,
+            &self
+                .batch
+                .batch_buffers()
+                .iter()
+                .map(|x| x.borrow_vertex_buffer())
+                .collect::<Vec<_>>(),
+            self.batch.triangle_count(),
+        );
+    }
+}
---
+++ examples/launch.rs
@@ -0,0 +1,27 @@
+use crate::app::hello_world_scene::HelloWorldScene;
+use gfx_hal::window::Extent2D;
+use std::env;
+use tearchan::core::engine::Engine;
+use tearchan::core::engine_config::StartupConfigBuilder;
+use tearchan::core::screen::ScreenMode;
+
+pub mod app;
+
+fn main() {
+    env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
+    let startup_config = StartupConfigBuilder::default()
+        .application_name("test".to_string())
+        .screen_mode(ScreenMode::Windowed {
+            resolutions: vec![Extent2D {
+                width: 600,
+                height: 400,
+            }],
+        })
+        .scene_creator(HelloWorldScene::creator())
+        .build()
+        .unwrap();
+
+    Engine::new(startup_config).run();
+}
