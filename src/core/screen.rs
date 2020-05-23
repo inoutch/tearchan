3ed668f6a37cff204c910df0960d2ea0d32b954b
inoutch <akiandaki@gmail.com>
2020-05-23 16:00:57 +0900

    feat(tearchan): Add creator of single windowed screen mode

--- src/core/screen.rs
+++ src/core/screen.rs
@@ -1,7 +1,14 @@
+use crate::core::screen::ScreenMode::Windowed;
 use gfx_hal::window::Extent2D;
 
 #[derive(Clone)]
 pub enum ScreenMode {
     FullScreenWindow,
     Windowed { resolutions: Vec<Extent2D> },
+}
+
+pub fn create_single_windowed(width: u32, height: u32) -> ScreenMode {
+    Windowed {
+        resolutions: vec![Extent2D { width, height }],
+    }
 }
