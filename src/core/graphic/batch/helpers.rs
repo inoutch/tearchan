98447eb42f870ce2e12e77dfef1cce51f085558d
inoutch <akiandaki@gmail.com>
2020-07-17 05:59:51 +0900

    fix(tearchan): Fix to keep the original value when remaking the buffer.

--- src/core/graphic/batch/helpers.rs
+++ src/core/graphic/batch/helpers.rs
@@ -1,4 +1,5 @@
 use crate::core::graphic::batch::batch_buffer::BatchBuffer;
+use crate::core::graphic::hal::buffer_interface::{BufferInterface, BufferMappedMemoryInterface};
 use crate::core::graphic::hal::index_buffer::IndexBufferCommon;
 use crate::core::graphic::hal::renderer_api::RendererApiCommon;
 use crate::core::graphic::hal::vertex_buffer::VertexBufferCommon;
@@ -10,13 +11,22 @@ const DEFAULT_VERTEX_BUFFER_SIZE: usize = 1000;
 pub fn create_index_batch_buffer<B: Backend>(
     api: &RendererApiCommon<B>,
 ) -> BatchBuffer<IndexBufferCommon<B>> {
-    // TODO: Let the buffer be copied
+    // TODO: Improve performance
     BatchBuffer::new(
         api.create_index_buffer(&[0; DEFAULT_INDEX_BUFFER_SIZE]),
         |buffer, size| {
-            buffer
+            let new_buffer = buffer
                 .create_index_buffer(&vec![0; size])
-                .expect("device is dropped")
+                .expect("device is dropped");
+            let src = buffer.open(0, buffer.size());
+            let mut dst = new_buffer.open(0, buffer.size());
+
+            for i in 0..buffer.size() {
+                dst.set(src.get(i), i);
+            }
+            buffer.close(src);
+            new_buffer.close(dst);
+            new_buffer
         },
     )
 }
@@ -24,13 +34,22 @@ pub fn create_index_batch_buffer<B: Backend>(
 pub fn create_vertex_batch_buffer<B: Backend>(
     api: &RendererApiCommon<B>,
 ) -> BatchBuffer<VertexBufferCommon<B>> {
-    // TODO: Let the buffer be copied
+    // TODO: Improve performance
     BatchBuffer::new(
         api.create_vertex_buffer(&[0.0f32; DEFAULT_VERTEX_BUFFER_SIZE]),
         |buffer, size| {
-            buffer
+            let new_buffer = buffer
                 .create_vertex_buffer(&vec![0.0f32; size])
-                .expect("device is dropped")
+                .expect("device is dropped");
+            let src = buffer.open(0, buffer.size());
+            let mut dst = new_buffer.open(0, buffer.size());
+
+            for i in 0..buffer.size() {
+                dst.set(src.get(i), i);
+            }
+            buffer.close(src);
+            new_buffer.close(dst);
+            new_buffer
         },
     )
 }
