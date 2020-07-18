e9005bba19725a401e000917ba826e3665724ee1
inoutch <akiandaki@gmail.com>
2020-07-18 17:04:47 +0900

    feat(tearchan): Remove old batch files.

--- src/core/graphic/batch/batch_bundle.rs
+++
@@ -1,8 +0,0 @@
-use crate::core::graphic::batch::batch_buffer::BatchBuffer;
-
-pub struct BatchBundle<TBatchBuffer: BatchBuffer> {
-    pub stride: u32,
-    pub batch_buffer: TBatchBuffer,
-}
-
-impl<TBatchBuffer: BatchBuffer> BatchBundle<TBatchBuffer> {}
