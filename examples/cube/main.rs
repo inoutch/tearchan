7a7e173a3e41de5c0b6e27c02c3020c890ffa92d
inoutch <akiandaki@gmail.com>
2020-09-25 00:23:32 +0900

    feat(teachan-example): Add env logger.

--- examples/cube/main.rs
+++ examples/cube/main.rs
@@ -1,5 +1,6 @@
 use crate::cube_scene::CubeScene;
 use nalgebra_glm::vec2;
+use std::env;
 use tearchan::engine::Engine;
 use tearchan::engine_config::StartupConfigBuilder;
 use tearchan_graphics::screen::ScreenMode;
@@ -7,6 +8,9 @@ use tearchan_graphics::screen::ScreenMode;
 pub mod cube_scene;
 
 fn main() {
+    env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
     let startup_config = StartupConfigBuilder::default()
         .application_name("cube".to_string())
         .screen_mode(ScreenMode::Windowed {
