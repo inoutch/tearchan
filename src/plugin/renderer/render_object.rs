4c3b2cb0a4dc8c06756f2c1555bcea37d99ddfe4
inoutch <akiandaki@gmail.com>
2020-10-04 14:37:24 +0900

    fix(tearchan): Add default function.

--- src/plugin/renderer/render_object.rs
+++ src/plugin/renderer/render_object.rs
@@ -2,5 +2,7 @@ use tearchan_core::game::object::game_object_base::GameObjectBase;
 use tearchan_graphics::batch::batch_command_queue::BatchCommandQueue;
 
 pub trait RenderObject: GameObjectBase {
-    fn attach_queue(&mut self, queue: BatchCommandQueue);
+    fn attach_queue(&mut self, _queue: BatchCommandQueue) {
+        unimplemented!()
+    }
 }
