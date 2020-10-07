d6ce7e2226eca2b781ad1a5168a346ebe241c450
inoutch <akiandaki@gmail.com>
2020-10-07 13:39:12 +0900

    refactor(tearchan): Unify name of render object.

--- src/plugin/renderer/billboard_renderer/billboard_render_object.rs
+++ src/plugin/renderer/billboard_renderer/billboard_render_object.rs
@@ -1,7 +1,7 @@
 use crate::plugin::renderer::billboard_renderer::billboard_command_queue::BillboardCommandQueue;
 use tearchan_core::game::object::game_object_base::GameObjectBase;
 
-pub trait BillboardObject: GameObjectBase {
+pub trait BillboardRenderObject: GameObjectBase {
     fn attach_queue(&mut self, queue: BillboardCommandQueue);
 
     fn detach(&mut self);
