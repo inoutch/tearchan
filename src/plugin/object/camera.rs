896bddefd1dbfdf2d508b1f6de66050da8cf8f59
inoutch <akiandaki@gmail.com>
2020-10-07 13:35:12 +0900

    fix(tearchan): Add camera object.

--- src/plugin/object/camera.rs
+++ src/plugin/object/camera.rs
@@ -2,17 +2,24 @@ use intertrait::cast_to;
 use tearchan_core::game::object::game_object_base::GameObjectBase;
 use tearchan_graphics::camera::camera_2d::Camera2D;
 use tearchan_graphics::camera::camera_3d::Camera3D;
+use tearchan_graphics::camera::Camera;
+
+pub trait CameraObject: GameObjectBase {
+    fn label(&self) -> &str;
+
+    fn camera(&self) -> &Camera;
+}
 
 pub trait Camera2DObject: GameObjectBase {
     fn label(&self) -> &str;
 
-    fn camera(&self) -> &Camera2D;
+    fn camera2d(&self) -> &Camera2D;
 }
 
 pub trait Camera3DObject: GameObjectBase {
     fn label(&self) -> &str;
 
-    fn camera(&self) -> &Camera3D;
+    fn camera3d(&self) -> &Camera3D;
 }
 
 pub struct Camera2DDefaultObject {
@@ -35,8 +42,19 @@ impl Camera2DObject for Camera2DDefaultObject {
         &self.label
     }
 
-    fn camera(&self) -> &Camera2D {
+    fn camera2d(&self) -> &Camera2D {
         &self.camera
+    }
+}
+
+#[cast_to]
+impl CameraObject for Camera2DDefaultObject {
+    fn label(&self) -> &str {
+        &self.label
+    }
+
+    fn camera(&self) -> &Camera {
+        self.camera.base()
     }
 }
 
@@ -60,7 +78,18 @@ impl Camera3DObject for Camera3DDefaultObject {
         &self.label
     }
 
-    fn camera(&self) -> &Camera3D {
+    fn camera3d(&self) -> &Camera3D {
         &self.camera
+    }
+}
+
+#[cast_to]
+impl CameraObject for Camera3DDefaultObject {
+    fn label(&self) -> &str {
+        &self.label
+    }
+
+    fn camera(&self) -> &Camera {
+        self.camera.base()
     }
 }
