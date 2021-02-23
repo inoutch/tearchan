ee6499a89a9dd28bc7ecf566f262289ad460272d
inoutch <akiandaki@gmail.com>
2021-02-24 00:20:21 +0900

    feat(tearchan-gfx): Add texture

--- tearchan-gfx/src/lib.rs
+++ tearchan-gfx/src/lib.rs
@@ -4,3 +4,4 @@ pub mod camera;
 pub mod context;
 pub mod image;
 pub mod renderer;
+pub mod texture;
---
+++ tearchan-gfx/src/texture.rs
@@ -0,0 +1,77 @@
+pub struct Texture {
+    texture: wgpu::Texture,
+    view: wgpu::TextureView,
+    sampler: wgpu::Sampler,
+    format: wgpu::TextureFormat,
+}
+
+impl Texture {
+    pub fn new(
+        texture: wgpu::Texture,
+        view: wgpu::TextureView,
+        sampler: wgpu::Sampler,
+        format: wgpu::TextureFormat,
+    ) -> Self {
+        Texture {
+            texture,
+            view,
+            sampler,
+            format,
+        }
+    }
+
+    pub fn new_depth_texture(device: &wgpu::Device, width: u32, height: u32, label: &str) -> Self {
+        let format = wgpu::TextureFormat::Depth32Float;
+        let size = wgpu::Extent3d {
+            width,
+            height,
+            depth: 1,
+        };
+        let texture_desc = wgpu::TextureDescriptor {
+            label: Some(label),
+            size,
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format,
+            usage: wgpu::TextureUsage::RENDER_ATTACHMENT | wgpu::TextureUsage::SAMPLED,
+        };
+        let texture = device.create_texture(&texture_desc);
+        let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let sampler = device.create_sampler(&wgpu::SamplerDescriptor {
+            address_mode_u: wgpu::AddressMode::ClampToEdge,
+            address_mode_v: wgpu::AddressMode::ClampToEdge,
+            address_mode_w: wgpu::AddressMode::ClampToEdge,
+            mag_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Linear,
+            mipmap_filter: wgpu::FilterMode::Nearest,
+            compare: Some(wgpu::CompareFunction::LessEqual),
+            lod_min_clamp: -100.0,
+            lod_max_clamp: 100.0,
+            ..Default::default()
+        });
+
+        Texture {
+            texture,
+            view,
+            sampler,
+            format,
+        }
+    }
+
+    pub fn texture(&self) -> &wgpu::Texture {
+        &self.texture
+    }
+
+    pub fn view(&self) -> &wgpu::TextureView {
+        &self.view
+    }
+
+    pub fn sampler(&self) -> &wgpu::Sampler {
+        &self.sampler
+    }
+
+    pub fn format(&self) -> wgpu::TextureFormat {
+        self.format
+    }
+}
