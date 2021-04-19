5872a7380feaca1a08f34767efdf90d8dcbe745f
inoutch <akiandaki@gmail.com>
2021-04-20 04:44:19 +0900

    feat(tearchan): Add as_any function to scene option.

--- src/scene/factory.rs
+++ src/scene/factory.rs
@@ -2,7 +2,9 @@ use crate::scene::context::SceneContext;
 use crate::scene::Scene;
 use std::any::Any;
 
-pub trait SceneOption: Any {}
+pub trait SceneOption: Any {
+    fn as_any(&self) -> &dyn Any;
+}
 
 pub type SceneFactory =
     fn(scene_context: &mut SceneContext, option: Option<Box<dyn SceneOption>>) -> Box<dyn Scene>;
