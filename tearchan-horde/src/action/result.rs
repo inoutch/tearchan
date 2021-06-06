9bb27fb45ddc62d63ddb7da741bff6770bbb1e2d
inoutch <akiandaki@gmail.com>
2021-06-06 15:24:08 +0900

    feat(tearchan-horde): Implement so that values can be retrieved for each type

--- tearchan-horde/src/action/result.rs
+++ tearchan-horde/src/action/result.rs
@@ -13,3 +13,32 @@ pub enum ActionResult<T> {
         action: Arc<Action<T>>,
     },
 }
+
+impl<T> ActionResult<T> {
+    pub fn get_start(&self) -> Option<&Arc<Action<T>>> {
+        match self {
+            ActionResult::Start { action } => Some(action),
+            ActionResult::Update { .. } => None,
+            ActionResult::End { .. } => None,
+        }
+    }
+
+    pub fn get_update(&self) -> Option<(&Arc<Action<T>>, u64)> {
+        match self {
+            ActionResult::Start { .. } => None,
+            ActionResult::Update {
+                action,
+                current_time,
+            } => Some((action, *current_time)),
+            ActionResult::End { .. } => None,
+        }
+    }
+
+    pub fn get_end(&self) -> Option<&Arc<Action<T>>> {
+        match self {
+            ActionResult::Start { .. } => None,
+            ActionResult::Update { .. } => None,
+            ActionResult::End { action } => Some(action),
+        }
+    }
+}
