b6a0e7d3c424a8e6d648bb001bd20eb30a54ea1c
inoutch <akiandaki@gmail.com>
2022-02-08 14:06:45 +0900

    feat(tearchan-horde): Add debug to JobResult

--- tearchan-horde/src/job/result.rs
+++ tearchan-horde/src/job/result.rs
@@ -2,6 +2,7 @@ use crate::action::manager::TimeMilliseconds;
 
 pub type ProgressState<ActionState> = (ActionState, TimeMilliseconds);
 
+#[derive(Debug)]
 pub struct JobResult<Job, ActionState> {
     pub creators: Vec<Job>,
     pub states: Vec<ProgressState<ActionState>>,
